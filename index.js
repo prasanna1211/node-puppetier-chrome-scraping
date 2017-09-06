8408f958d612a466ffa24830baf75644d4b21990
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-06 14:08:15 +0530

    tried with different API

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ async function run() {
   });
   const page = await browser.newPage();
 
-  await page.goto('https://google.com');
+  await page.goto('https://www.google.com/ncr');
+  await page.goto('https://www.google.co.uk/');
+
+  // await page.goto('https://www.google.com/ncr');
 
   page.focus('input[title="Search"]');
+
   await page.type('React JS development company', { delay: 100 });
+
 
   const submit = await page.$('input[value="Google Search"]');
   await submit.click();
 
-  await page.waitFor(10000);
+  await page.waitForNavigation();
+
+  const url = await page.url();
+  const text = await page.plainText();
+  console.log(text);
+  await page.waitFor(40000);
   await page.screenshot({path: 'screenshots/github.png'});
 
   browser.close();
